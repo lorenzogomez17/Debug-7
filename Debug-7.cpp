edd415376528bd00fed3f7444e7c539f308ef240
lorenzogomez17 <72454940+lorenzogomez17@users.noreply.github.com>
2020-10-20 10:18:09 -0500

    Update Debug-7.cpp

--- Debug-7.cpp
+++ Debug-7.cpp
@@ -7,9 +7,14 @@ using namespace std;
 
 // This code is designed to approximate the square root of 2.
 // Remember that the square root of 2 is simply the number that square to 2.
-double square(int x){
-    cout << x*x;
-    return 0;
+//Switched the int to double so the output can be a decimal and not a intiger.
+double square(double x){
+  //took cout out because it has no need and replaced it with the var y to do X*X
+  double y = x*x;
+  //returned y so it can be used and not 0 beacause that goes at the end of the code 
+  return y;
+  //  cout << x*x;
+  //  return 0;
 }
 double lowApprox = 1;
 double highApprox = 2;
@@ -27,11 +32,11 @@ if (square(lowApprox)<2 && square(highApprox)>2){
         if (square(middle)>2){
             highApprox = middle;
         }
-	if (square(middle)==2){
+	 if (square(middle)==2){
 	    break;
 	}        
     }
 }
-cout << "The square root of 2 is approximately " << middle;
+cout << " The square root of 2 is approximately " << middle;
 	return 0;
 }
